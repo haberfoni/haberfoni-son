62484940c5e3ad967c3a9dc6ce74c5cd55ae7504
ahmetcansertce-svg <ahmetcansertce@hotmail.com>
2025-12-17 13:39:33 +0300

    haberfoni-son

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Self-hosted fonts
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/900.css';
 import { HelmetProvider } from 'react-helmet-async';
 import { SiteSettingsProvider } from './context/SiteSettingsContext';
 import App from './App.jsx'
